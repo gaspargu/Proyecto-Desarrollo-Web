d568d5cba4ef9c9ea78db523726ec64f80403a87
gaspargu <gasp.gumucio@gmail.com>
2023-04-10 21:17:00 -0400

    Display de regiones y comunas listo

--- js/agregar-donacion.js
+++ js/agregar-donacion.js
@@ -2,6 +2,7 @@ import { data } from './region_comuna.js';
 
 let data_regiones = data.regiones
 
+
 let regionInput = document.getElementById("region")
 let comunaInput = document.getElementById("comuna")
 let callenumeroInput = document.getElementById("calle-numero")
@@ -22,12 +23,111 @@ populateSelect(regionInput,data_regiones);
 // recive el string con el valor de la region elegida por el usuario y popula el select de comuna 
 // con las comunas correspondiente
 const populateComuna = (event) => {
-  const selectedValue = event.target.value;
-  console.log(selectedValue === '1')
+  for (let i = comunaInput.options.length - 1; i > 0; i--) {
+    comunaInput.options[i].remove();
+  }
+  let selectedValue = parseInt(event.target.value);
+  let comunas = data_regiones.find(r => r.id === selectedValue).comunas
+  populateSelect(comunaInput,comunas)
 }
 
-
 regionInput.addEventListener('change', populateComuna);
+
+const validateEmail = (email) => {
+  if (!email) return false;
+  // length validation
+  let lengthValid = email.length > 15;
+
+  // format validation
+  let re = /^[\w.]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  let formatValid = re.test(email);
+
+  // return logic AND of validations.
+  return lengthValid && formatValid;
+};
+
+const validatePhoneNumber = (phoneNumber) => {
+  if (!phoneNumber) return false;
+  // length validation
+  let lengthValid = phoneNumber.length >= 8;
+
+  // format validation
+  let re = /^[0-9]+$/;
+  let formatValid = re.test(phoneNumber);
+
+  // return logic AND of validations.
+  return lengthValid && formatValid;
+};
+
+const validateFiles = (files) => {
+  if (!files) return false;
+
+  // number of files validation
+  let lengthValid = 1 <= files.length && files.length <= 3;
+
+  // file type validation
+  let typeValid = true;
+
+  for (const file of files) {
+    // file.type should be "image/<foo>" or "application/pdf"
+    let fileFamily = file.type.split("/")[0];
+    typeValid &&= fileFamily == "image" || file.type == "application/pdf";
+  }
+
+  // return logic AND of validations.
+  return lengthValid && typeValid;
+};
+
+const validateForm = () => {
+  // get elements from DOM by using form's name.
+  let myForm = document.forms["myForm"];
+  let email = myForm["email"].value;
+  let phoneNumber = myForm["phone"].value;
+  let files = myForm["files"].files;
+
+  // validation auxiliary variables and function.
+  let invalidInputs = [];
+  let isValid = true;
+  const setInvalidInput = (inputName) => {
+    invalidInputs.push(inputName);
+    isValid &&= false;
+  };
+
+  // validation logic
+  if (!validateEmail(email)) {
+    setInvalidInput("Email");
+  }
+  if (!validatePhoneNumber(phoneNumber)) {
+    setInvalidInput("Phone number");
+  }
+  if (!validateFiles(files)) {
+    setInvalidInput("Picture(s)");
+  }
+
+  // finally display validation
+  let validationBox = document.getElementById("val-box");
+  let validationMessageElem = document.getElementById("val-msg");
+  let validationListElem = document.getElementById("val-list");
+
+  if (!isValid) {
+    validationListElem.textContent = "";
+    // add invalid elements to val-list element.
+    for (input of invalidInputs) {
+      let listElement = document.createElement("li");
+      listElement.innerText = input;
+      validationListElem.append(listElement);
+    }
+    // set val-msg
+    validationMessageElem.innerText = "Los siguiente campos son invalidos:";
+
+    // make validation prompt visible
+    validationBox.hidden = false;
+  } else {
+    myForm.submit();
+  }
+};
+
+let submitBtn = document.getElementById("submit-btn");
 
 
 
